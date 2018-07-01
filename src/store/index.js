33db1520a92168e0be723da42f648c36ca7aa982
revyakins <scoundrel1992@gmail.com>
2018-07-02 00:03:31 +0300

    delete readme.md

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,6 @@ import { createStore, applyMiddleware } from 'redux';
 import reducer from '../reducesrs';
 import promiseMiddleware from '../middleware/promises';
 
-
 const createdStoreWithMiddleware = applyMiddleware(promiseMiddleware)(createStore)
 
 const store = createdStoreWithMiddleware(reducer, {
